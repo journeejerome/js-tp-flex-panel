7de0b2f8e9c8d015cb7a4f4d03bc9d38d7afaf97
Journée Jérôme <jerome.journee@outlook.be>
2019-12-05 13:56:41 +0100

    :white_check_mark: Js Done

---
+++ src/js/main.js
@@ -0,0 +1,29 @@
+(function () {
+    const FlexPanel = {
+        init(){
+            document.documentElement.className = "js-enabled";
+            this.panel = document.querySelectorAll(".panel");
+            for(const element of this.panel){
+                element.addEventListener('click', ()=>{
+                    this.animatePanel(element, this.panel);
+                });
+                element.addEventListener("transitionend", ()=>{
+                    this.addText(element);
+                });
+            }
+        },
+        animatePanel(element, panel){
+            for(const elmnt of panel){
+                elmnt.classList.remove('open');
+                elmnt.classList.remove('open-active');
+            }
+            element.classList.add("open");
+        },
+        addText(element){
+            if(element.classList.contains("open")){
+                element.classList.add("open-active");
+            }
+        }
+    }
+    FlexPanel.init();
+})();
